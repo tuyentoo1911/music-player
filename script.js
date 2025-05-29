6b8ee828b140c1eae96a72fe22f613489c1891ea
tuyentoo1911 <trantuyento2k4@gmail.com>
2025-05-29 14:25:00 +0700

    push music

---
+++ script.js
@@ -0,0 +1,26 @@
+const $ = document.querySelector.bind(document)
+const $$ = document.querySelectorAll.bind(document)
+
+
+
+
+const songs = [
+    {
+        name: 'Song 1',
+        singer: 'Artist 1',
+        image: 'https://i.ytimg.com/vi/jTLhQf5KJSc/maxresdefault.jpg',
+        audio: 'assets/audio/song1.mp3'
+    },
+    {
+        name: 'Song 2',
+        singer: 'Artist 2',
+        image: 'https://i.ytimg.com/vi/jTLhQf5KJSc/maxresdefault.jpg', 
+        audio: 'assets/audio/song2.mp3'
+    },
+    {
+        name: 'Song 3',
+        singer: 'Artist 3',
+        image: 'https://i.ytimg.com/vi/jTLhQf5KJSc/maxresdefault.jpg',
+        audio: 'assets/audio/song3.mp3'
+    }
+]
